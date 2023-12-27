b1e82b001bb57976b94ea86e9e2a9b8ca565ce35
TheCeco <rapidfire114@gmail.com>
2023-12-27 23:07:01 +0200

    no message

---
+++ to_do_list/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <input v-model="username">
+  <button @click="searchProfile">Search</button>
+</template>
+
+<script>
+import profiles from './profiles.json'
+
+export default {
+  data () {
+    return {
+      profiles: profiles
+    }
+  },
+  methods: {
+    searchProfile () {
+      profiles.forEach(profile => {
+        if (profile.username.includes(this.username)) {
+          console.log(profile)
+        }
+      })
+    }
+  }
+}
+
+</script>
