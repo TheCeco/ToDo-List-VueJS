143fad7c8338725d115ea2589e915786f4fa1f20
TheCeco <rapidfire114@gmail.com>
2023-12-28 09:01:02 +0200

    work in progress

--- to_do_list/src/App.vue
+++ to_do_list/src/App.vue
@@ -3,8 +3,13 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <input v-model="username">
-  <button @click="searchProfile">Search</button>
+  <div id="app">
+    <input v-model="username" @input="searchProfile">
+    <button @click="searchProfile">Search</button>
+    <ul>
+      <li v-for="(profile, index) in this.result" :key="index">{{ profile.username }}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -13,16 +18,17 @@ import profiles from './profiles.json'
 export default {
   data () {
     return {
-      profiles: profiles
+      result: []
     }
   },
   methods: {
     searchProfile () {
       profiles.forEach(profile => {
         if (profile.username.includes(this.username)) {
-          console.log(profile)
+          this.result.push(profile)
         }
       })
+      return this.result
     }
   }
 }
